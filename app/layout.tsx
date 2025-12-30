4bbad3c604a660a4a3a0c321fefe9573a1329589
nairnavya <nairnavya@gmail.com>
2025-12-30 11:44:51 -0600

    Update tab title and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,10 +27,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/* <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-       */}
+      <head>
+        <title>Navya Nair</title>
+        <link rel="icon" href="/favicon-32x32.png" />
+      </head>
        <body className={inter.className}>
         {children}
       </body>
